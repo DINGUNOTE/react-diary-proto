91dd28df08356d6d910aa3584a1a12e45fd6800d
web.ljg <web.ljg@gmail.com>
2022-05-06 10:40:41 +0900

    # 일기 리스트에 일기 추가 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,43 +1,61 @@
+import { useState, useRef } from 'react';
 import DiaryEditor from './components/DiaryEditor';
 import DiaryList from './components/DiaryList';
 import './App.css';
 
-const dummyList = [
-  {
-    id: 1,
-    author: '홍길동',
-    content: '오늘의 일기1',
-    emotion: 5,
-    createdDate: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: '아무개',
-    content: '오늘의 일기2',
-    emotion: 5,
-    createdDate: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: '이순신',
-    content: '오늘의 일기3',
-    emotion: 5,
-    createdDate: new Date().getTime(),
-  },
-  {
-    id: 4,
-    author: '이성계',
-    content: '오늘의 일기4',
-    emotion: 5,
-    createdDate: new Date().getTime(),
-  },
-];
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: '홍길동',
+//     content: '오늘의 일기1',
+//     emotion: 5,
+//     createdDate: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: '아무개',
+//     content: '오늘의 일기2',
+//     emotion: 5,
+//     createdDate: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: '이순신',
+//     content: '오늘의 일기3',
+//     emotion: 5,
+//     createdDate: new Date().getTime(),
+//   },
+//   {
+//     id: 4,
+//     author: '이성계',
+//     content: '오늘의 일기4',
+//     emotion: 5,
+//     createdDate: new Date().getTime(),
+//   },
+// ];
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const createdDate = new Date().getTime();
+    const newItem = {
+      id: dataId.current,
+      author,
+      content,
+      emotion,
+      createdDate,
+    };
+    dataId.currnet += 1;
+    setData([newItem, ...data]);
+  };
+
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
