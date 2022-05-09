d34e1b1d8b237d85994e72a114ac80e882a8183f
web.ljg <web.ljg@gmail.com>
2022-05-09 18:08:19 +0900

    # useEffect를 사용해서 마운트 시 API 호출 기능 구현, useMemo를 사용해서 연산 값 재사용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect, useMemo } from 'react';
 import DiaryEditor from './components/DiaryEditor';
 import DiaryList from './components/DiaryList';
 import './App.css';
 
-function App() {
+const App = () => {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const response = await fetch(
+      'https://jsonplaceholder.typicode.com/comments',
+    ).then(response => response.json());
+
+    const initData = response.slice(0, 20).map(it => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        createdDate: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const createdDate = new Date().getTime();
@@ -34,12 +56,27 @@ function App() {
     setData(newDiaryList);
   };
 
+  const getDiaryAnalysis = useMemo(() => {
+    console.log('일기 분석 시작');
+
+    const goodCount = data.filter(it => it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]);
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length}</div>
+      <div>기분 좋은 일기 갯수 : {goodCount}</div>
+      <div>기분 나쁜 일기 갯수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio}</div>
       <DiaryList diaryList={data} onEdit={onEdit} onRemove={onRemove} />
     </div>
   );
-}
+};
 
 export default App;
